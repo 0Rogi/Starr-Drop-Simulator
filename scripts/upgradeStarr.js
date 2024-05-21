f53c16fc182a19cfa08d3856d3963e29e8a54af8
0Rogi <0rogi@noreply.com>
2024-05-21 08:36:31 +0200

    Starr Drop Now Works

--- scripts/upgradeStarr.js
+++ scripts/upgradeStarr.js
@@ -1,10 +1,77 @@
 const starr = document.getElementById(`starr`);
+let opened = false;
+
+setInterval(() => {
+    if(opened) {
+        document.addEventListener(`click`, () => {
+            window.location.reload();
+        })
+    }
+}, 500);
 
 starr.addEventListener(`click`, e => {
     const currentUpgrade = document.getElementById(`current`);
 
     if (!currentUpgrade) {
-        // TODO Aprire Starr Drop
+        //? Get the rarity
+        const currentRarity = document.getElementById(`rarityText`);
+
+        //? Calibrating Prices
+        let prices = [];
+        switch (currentRarity.textContent.toLowerCase()) {
+            case `rare`: {
+                prices = [`50x Coins`, `25x Power Points`, `10x Credits`, `10x Bling`, `25x XP Doublers`];
+            } break;
+            case `super rare`: {
+                prices = [`100x Coins`, `50x Power Points`, `50x Credits`, `20x Bling`, `200x XP Doublers`];
+            } break;
+            case `epic`: {
+                prices = [`500x Coins`, `200x Power Points`, `100x Credits`, `50x Bling`, `500x XP Doublers`];
+            } break;
+            case `mythic`: {
+                prices = [`700x Coins`, `500x Power Points`, `200x Credits`, `100x Bling`, `700x XP Doublers`];
+            } break;
+            case `legendary`: {
+                prices = [`1000x Coins`, `1000x Power Points`, `500x Credits`, `200x Bling`, `1000x XP Doublers`];
+            } break;
+            default: {
+                prices = [`50x Coins`, `25x Power Points`, `10x Credits`, `10x Bling`, `25x XP Doublers`];
+            } break;
+        }
+
+        //? Choosing Price
+        const price = prices[Math.floor(Math.random() * prices.length)];
+
+        //? Displaying Price
+        document.getElementById(`container`).remove();
+        document.body.style.backgroundImage = 'radial-gradient(white 10%, cyan, blue)';
+
+        const div = document.createElement(`div`);
+        div.className = `container`;
+        div.id = `container`;
+        document.body.appendChild(div);
+        
+        const img = document.createElement(`img`);
+        const text = document.createElement(`h2`);
+        text.textContent = price;
+
+        if (price.includes(`Coins`)) {
+            img.src = `./assets/images/prices/coins.png`;
+        } else if (price.includes(`Power Points`)) {
+            img.src = `./assets/images/prices/power_points.png`;
+        } else if (price.includes(`Credits`)) {
+            img.src = `./assets/images/prices/credits.png`;
+        } else if (price.includes(`Bling`)) {
+            img.src = `./assets/images/prices/blings.png`;
+        } else if (price.includes(`XP Doublers`)) {
+            img.src = `./assets/images/prices/xp_doublers.png`;
+        }
+
+        document.getElementById(`container`).appendChild(img);
+        document.getElementById(`container`).appendChild(text);
+        
+        opened = true;
+
         return;
     }
 
@@ -56,11 +123,11 @@ starr.addEventListener(`click`, e => {
         nextUpgrade.id = `current`;
     } else {
         let circle = document.getElementsByClassName(`circle`);
-        for(let i = 0; i < circle.length; i++) {
+        for (let i = 0; i < circle.length; i++) {
             circle[i].style.visibility = `hidden`;
         }
         document.getElementById(`upgradeText`).textContent = `TAP TO OPEN`;
-        document.getElementById(`starr`).style.width = `32%`
-        document.getElementById(`starr`).style.height = `32%`
+        // document.getElementById(`starr`).style.width = `32%`
+        // document.getElementById(`starr`).style.height = `32%`
     }
 });
